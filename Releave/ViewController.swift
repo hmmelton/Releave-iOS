10a966f5ca9c167266660ab4bc9e30d619f02a40
Harrison Melton <harrisonmelton@Harrisons-MacBook-Pro.local>
2017-09-02 20:11:32 -0700

    Initial Commit

---
+++ Releave/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Releave
+//
+//  Created by Harrison Melton on 9/2/17.
+//  Copyright © 2017 Harrison Melton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
