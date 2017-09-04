6edc6f8c0c62d98a1fac50aff224ce9a7aa5a70e
Harrison Melton <harrisonmelton@Harrisons-MacBook-Pro.local>
2017-09-03 18:33:33 -0700

    added images & completed 2nd page UI

--- Releave/AddRestroomViewController.swift
+++ Releave/AddRestroomViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class AddRestroomViewController: UIViewController {
 
@@ -14,6 +15,9 @@ class AddRestroomViewController: UIViewController {
     @IBAction func onCancelPressed(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
     }
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var infoView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
