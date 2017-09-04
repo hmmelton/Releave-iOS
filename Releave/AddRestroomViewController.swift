8cc7026f4cd6462529b594a7547ae0c001fe1853
Harrison Melton <harrisonmelton@Harrisons-MacBook-Pro.local>
2017-09-03 17:32:16 -0700

    added second page

---
+++ Releave/AddRestroomViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddRestroomViewController.swift
+//  Releave
+//
+//  Created by Harrison Melton on 9/3/17.
+//  Copyright © 2017 Harrison Melton. All rights reserved.
+//
+
+import UIKit
+
+class AddRestroomViewController: UIViewController {
+
+    // When 'Cancel' button is pressed, dismiss the view controller
+    @IBAction func onCancelPressed(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
