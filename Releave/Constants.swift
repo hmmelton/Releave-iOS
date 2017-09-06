c8947c2194c75f9a550ae15ad08e64aaa22006c2
Harrison Melton <harrisonmelton@Harrisons-MacBook-Pro.local>
2017-09-06 10:51:22 -0700

    moved constants into Constants file

--- Releave/Constants.swift
+++ Releave/Constants.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+// This struct holds constants for the API
+struct ReleaveApi {
+    private static let root = "https://releave-65906.herokuapp.com"
+    
+    // API's endpoints
+    struct Endpoints {
+        static let login = root + "/login"
+        static let charge = root + "/charge"
+        static let users = root + "/users"
+        static let restrooms = root + "/restrooms"
+        static let areaRestrooms = root + "/area_restrooms"
+    }
+    
+    // Keys used in requests
+    struct RequestKeys {
+        static let authKey = "api_key"
+        static let id = "id"
+        static let facebookId = "fb_id"
+        static let stripeToken = "stripe_token"
+        static let chargeAmount = "amount"
+        static let minLat = "min_lat"
+        static let maxLat = "max_lat"
+        static let minLng = "min_lng"
+        static let maxLng = "max_lng"
+    }
+}
+
