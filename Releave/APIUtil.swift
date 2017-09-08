8b9191a9aa8d7521fc58defb1d0bf80b0e6114cf
Harrison Melton <harrisonmelton@Harrisons-MacBook-Pro.local>
2017-09-07 18:18:56 -0700

    added and updated API request methods and tests to reflect new API format

--- Releave/APIUtil.swift
+++ Releave/APIUtil.swift
@@ -12,30 +12,79 @@ import SwiftyJSON
 
 class APIUtil {
     
-    // The only header that is set for all requests is the API key
-    let headers: [String : String] = [ReleaveApi.RequestKeys.authHeader : SecretKeys.authToken]
+    // Authorization header used by all requests
+    static let headers: [String:String] = [ReleaveApi.RequestKeys.authHeader : SecretKeys.authToken]
     
     // MARK: Users
     
     // This function gets an individual user
     static func getUser(_ id: String, completion: @escaping (User?, Error?) -> ()) {
-        // Create URL including query parameters
-        let url = URL(string: ReleaveApi.Endpoints.users + "?\(ReleaveApi.RequestKeys.id)=\(id)" )!
         
-        var urlRequest = URLRequest(url: url)
-        // Add Authorization header to URL
-        urlRequest.addValue(SecretKeys.authToken, forHTTPHeaderField: ReleaveApi.RequestKeys.authHeader)
+        // Format URL with query parameters
+        let url = "\(ReleaveApi.Endpoints.users)/\(id)"
         
         // Send GET request to server
-        Alamofire.request(urlRequest).responseJSON { response in
-            if let data = response.result.value as! Dictionary<String, Any>? {
-                completion(User(JSON(data)), nil)
+        Alamofire.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: headers)
+            .responseJSON { response in
+            
+            guard let result = response.result.value as! Dictionary<String, Any>? else {
+                // Could not fetch response data - return error to completion handler
+                print(response.error)
+                completion(nil, response.error)
+                return
             }
+                
+            // Convert result to User object and return to completion handler
+            print(result)
+            completion(User(JSON(result)), nil)
+        }
+    }
+    
+    // This function creates a new user
+    static func createUser(_ body: Parameters, completion: @escaping (User?, Error?) -> ()) {
+        
+        // Set up URL
+        let url = ReleaveApi.Endpoints.users
+        
+        // Send POST request to server
+        Alamofire.request(url, method: .post, parameters: body, encoding: URLEncoding.httpBody, headers: headers).responseJSON { response in
+            
+            guard let result = response.result.value as! Dictionary<String, Any>? else {
+                // Failure fetching status code - return error to completion handler
+                completion(nil, response.error)
+                return
+            }
+            
+            // Convert result to User object and return to completion handler
+            completion(User(JSON(result)), nil)
         }
     }
     
     // This function updates an individual user
-    static func updateUser(_ id: String, withUpdatedInfo body: Parameters, completion: @escaping (Bool?, Error?) -> ()) {
+    static func updateUser(_ id: String, withUpdatedInfo body: Parameters, completion: @escaping (User?, Error?) -> ()) {
+        
+        // Format URL with parameter
+        let url = "\(ReleaveApi.Endpoints.users)/\(id)"
+        
+        // Send PUT request to server
+        Alamofire.request(url, method: .put, parameters: body, encoding: URLEncoding.httpBody, headers: headers).responseJSON { response in
+            
+            guard let result = response.result.value as! Dictionary<String, Any>? else {
+                // Failure fetching status code - return error to completion handler
+                completion(nil, response.error)
+                return
+            }
+
+            // Convert result to User object and return to completion handler
+            completion(User(JSON(result)), nil)
+        }
+    }
+    
+    // This function deletes an individual user
+    static func deleteUser(_ id: String, completion: @escaping (Bool?, Error?) -> ()) {
+        
+        // Format URL with parameter
+        let url = "\(ReleaveApi.Endpoints.users)/\(id)"
         
     }
     
--- ReleaveTests/ReleaveAPITests.swift
+++ ReleaveTests/ReleaveAPITests.swift
@@ -23,7 +23,7 @@ class ReleaveAPITests: XCTestCase {
         super.tearDown()
     }
     
-    func testGetUser() {
+    func testGetUserSuccess() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
@@ -32,18 +32,19 @@ class ReleaveAPITests: XCTestCase {
         
         let promise = expectation(description: "Status: 200")
         
-        var error: Error?
         var result: User?
+        var error: Error?
         
         // 2. When
         APIUtil.getUser(id) { (r, e) in
-            error = e
             result = r
+            error = e
             
             promise.fulfill()
         }
         
-        waitForExpectations(timeout: 20, handler: nil)
+        //waitForExpectations(timeout: 60, handler: nil)
+        wait(for: [promise], timeout: 20)
         
         // 3. Then
         
@@ -54,11 +55,77 @@ class ReleaveAPITests: XCTestCase {
         // JSON format assertions
         XCTAssert(result!.firstName == "Harrison", "first_name was not 'Harrison'")
         XCTAssert(result!.lastName == "Melton", "last_name was not 'Melton'")
-        XCTAssert(result!.email == "hmmelton@comcast.net", "email was not 'hmmelton@comcast.net'")
+        XCTAssert(result!.email == "hmmelton95@gmail.com", "email was not 'hmmelton95@gmail.com'")
     }
     
-    func testUpdateUser() {
+    func testCreateUserSuccess() {
+        // 1. Given
         
+        let body = [
+            "first_name" : "Octavius",
+            "last_name" : "Rex",
+            "email" : "o.rex@gmail.com",
+            "facebook_id" : 3827238129393,
+            "paid" : true,
+            "paid_exp": 83892929,
+            "stripe_token" : "32kjjf3k2jlfkl32j39fdsak329hr3j2-12i"
+        ] as [String : Any]
+        
+        let promise = expectation(description: "Status 201")
+        
+        var result: User?
+        var error: Error?
+        
+        // 2. When
+        APIUtil.createUser(body) { (r, e) in
+            result = r
+            error = e
+            
+            promise.fulfill()
+        }
+        
+        //waitForExpectations(timeout: 20, handler: nil)
+        wait(for: [promise], timeout: 20)
+        
+        // 3. Then
+        XCTAssert(error == nil, "Error was not nil")
+        XCTAssert(result != nil, "Result was not true")
+        
+        XCTAssert(result!.firstName == "Octavius", "first_name was not 'Octavius'")
+        XCTAssert(result!.lastName == "Rex", "last_name was not 'Rex'")
+        XCTAssert(result!.email == "o.rex@gmail.com", "email was not 'o.rex@gmail.com'")
+        XCTAssert(result!.facebookId == "3827238129393", "facebook_id was not '3827238129393'")
+        XCTAssert(result!.paid!, "paid was not true")
+        XCTAssert(result!.paidExp! == 83892929, "paid_exp was not '83892929'")
+        XCTAssert(result!.stripeToken! == "32kjjf3k2jlfkl32j39fdsak329hr3j2-12i",
+                  "stripe_token was not '32kjjf3k2jlfkl32j39fdsak329hr3j2-12i'")
+    }
+    
+    func testUpdateUserEmailSuccess() {
+        // 1. Given
+        let id = "59af1dde420c050011b6b6bf"
+        
+        let promise = expectation(description: "Status: 200")
+        
+        var result: User?
+        var error: Error?
+        
+        // 2. When
+        APIUtil.updateUser(id, withUpdatedInfo: ["email" : "hmmelton95@gmail.com"]) { (r, e) in
+            result = r
+            error = e
+            
+            promise.fulfill()
+        }
+        
+        //waitForExpectations(timeout: 20, handler: nil)
+        wait(for: [promise], timeout: 20)
+        
+        // 3. Then
+        XCTAssert(error == nil, "Error was not nil")
+        XCTAssert(result != nil, "Result was nil")
+        print("\n\n\(result)\n\n")
+        XCTAssert(result!.email == "hmmelton95@gmail.com", "email was not hmmelton95@gmail.com")
     }
     
 }
